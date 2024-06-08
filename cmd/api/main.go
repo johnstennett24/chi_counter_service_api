2b75323c9bd36a7c5da660497138f0bff50091a0
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2024-06-08 13:07:04 -0500

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"chi_pos_counter_service_api/internal/server"
+	"fmt"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
